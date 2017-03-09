0ccedef215f4a963513e78fc59810cfa8eb0a423
Rachel Chang <juc62@pitt.edu>
2017-03-08 23:34:12 -0500

    update js

--- day6.js
+++ day6.js
@@ -1,10 +1,9 @@
-$(document).ready(function(){
-  function myMap() {
-      var mapCanvas = document.getElementById("map");
-      var mapOptions = {
-          center: new google.maps.LatLng(51.5, -0.2),
-          zoom: 10
-      };
-      var map = new google.maps.Map(mapCanvas, mapOptions);
-  };
-})
+
+function myMap() {
+    var mapCanvas = document.getElementById("map");
+    var mapOptions = {
+        center: new google.maps.LatLng(51.5, -0.2),
+        zoom: 10
+    };
+    var map = new google.maps.Map(mapCanvas, mapOptions);
+}
