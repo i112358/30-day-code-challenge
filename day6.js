ba0cde07b2b223d1db230eb99a77cb19fce5600e
Rachel Chang <juc62@pitt.edu>
2017-03-10 18:58:35 -0500

    add day6

--- day6.js
+++ day6.js
@@ -1,9 +1,8 @@
-
 function myMap() {
     var mapCanvas = document.getElementById("map");
     var mapOptions = {
-        center: new google.maps.LatLng(51.5, -0.2),
-        zoom: 10
+        center: new google.maps.LatLng(23.6, 120.0),
+        zoom: 7
     };
     var map = new google.maps.Map(mapCanvas, mapOptions);
 }
